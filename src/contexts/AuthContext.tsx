fb3286a8b25668aadf2a4c3fa9f89f02ec648fcf
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-11-15 11:43:34 -0300

    Changes

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,7 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
-import * as AppleAuthentication from "expo-apple-authentication";
+import { createContext, ReactNode, useState, useEffect } from "react";
 import * as Google from "expo-auth-session/providers/google";
+import * as AuthSessions from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
-import * as AuthSession from "expo-auth-session";
-import { Platform } from "react-native";
 import { api } from "@/services/api";
 
 WebBrowser.maybeCompleteAuthSession();
@@ -13,42 +11,33 @@ interface UserProps {
   avatarUrl: string;
 }
 
-export interface AuthContextDataProps {
+export interface AuthContexDataProps {
   user: UserProps;
+  singIn: () => Promise<void>;
   isUserLoading: boolean;
-  signIn: () => Promise<void>;
 }
 
 interface AuthProviderProps {
   children: ReactNode;
 }
 
-export const AuthContext = createContext({} as AuthContextDataProps);
+export const AuthContext = createContext({} as AuthContexDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
-  const [user, setUser] = useState<UserProps>({} as UserProps);
   const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
 
   const [request, response, promptAsync] = Google.useAuthRequest({
     clientId:
       "586631289098-6smf4olg6h8dgavredhb117n631ri5om.apps.googleusercontent.com",
-    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    redirectUri: AuthSessions.makeRedirectUri({ useProxy: true }),
     scopes: ["profile", "email"],
   });
 
-  async function signIn() {
+  async function singIn() {
     try {
       setIsUserLoading(true);
-      {
-        Platform.OS === "ios"
-          ? AppleAuthentication.signInAsync({
-              requestedScopes: [
-                AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-                AppleAuthentication.AppleAuthenticationScope.EMAIL,
-              ],
-            })
-          : await promptAsync();
-      }
+      await promptAsync();
     } catch (error) {
       console.log(error);
       throw error;
@@ -57,41 +46,35 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
     }
   }
 
-  async function signInWithGoogle(access_token: string) {
+  async function singInWithGoogle(access_token: string) {
     try {
       setIsUserLoading(true);
-      const tokenResponse = await api.post("/users", {
-        access_token,
-      });
+
+      const tokenResponse = await api.post("/users", { access_token });
       api.defaults.headers.common[
         "Authorization"
       ] = `Bearer ${tokenResponse.data.token}`;
+
       const userInfoResponse = await api.get("/me");
       setUser(userInfoResponse.data.user);
-      console.log("token de auth", access_token);
-      
     } catch (error) {
       console.log(error);
       throw error;
     } finally {
       setIsUserLoading(false);
     }
-  }
-
-  async function signInWithApple(access_token: string) {
-    console.log("token de auth", access_token);
   }
 
   useEffect(() => {
     if (response?.type === "success" && response.authentication?.accessToken) {
-      signInWithGoogle(response.authentication.accessToken);
+      singInWithGoogle(response.authentication.accessToken);
     }
-  }, []);
+  }, [response]);
 
   return (
     <AuthContext.Provider
       value={{
-        signIn,
+        singIn,
         isUserLoading,
         user,
       }}
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -6,36 +6,24 @@ import { useAuth } from "@/hooks/useAuth";
 import { Fontisto } from "@expo/vector-icons";
 
 import Logo from "../assets/logo.svg";
-import { Platform } from "react-native";
 
 export function SignIn() {
-  const { signIn, isUserLoading } = useAuth();
+  const { singIn, isUserLoading } = useAuth();
 
   return (
     <Center flex={1} bgColor="gray.900" padding={7}>
       <Logo width={212} height={40} />
-      {Platform.OS === "ios" ? (
-        <Button
-          type="APPLE"
-          title="CONTINUAR COM APPLE"
-          leftIcon={
-            <Icon as={Fontisto} name="apple" color="black" size="lg" mb="1.5" />
-          }
-          mt="12"
-          onPress={signIn}
-          isLoading={isUserLoading}
-        />
-      ) : (
-        <Button
-          type="SECONDARY"
-          title="CONTINUAR COM GOOGLE"
-          leftIcon={
-            <Icon as={Fontisto} name="google" color="white" size="lg" mb="1" />
-          }
-          mt="12"
-          onPress={signIn}
-        />
-      )}
+
+      <Button
+        type="SECONDARY"
+        title="CONTINUAR COM GOOGLE"
+        leftIcon={
+          <Icon as={Fontisto} name="google" color="white" size="lg" mb="1" />
+        }
+        mt="12"
+        onPress={singIn}
+        isLoading={isUserLoading}
+      />
       <Text
         color="white"
         fontSize="12"
